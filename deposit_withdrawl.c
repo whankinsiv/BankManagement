825320e512972e5d732563e8a31996a765882d47
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2021-03-27 23:48:25 -0700

    Update deposit_withdrawl.c

--- deposit_withdrawl.c
+++ deposit_withdrawl.c
@@ -1,4 +1,8 @@
 #include <stdio.h>
 void deposit_withdrawl(){
-  printf("Inside Deposit_withdrawl.c");
+  char client_first_name[30];
+  char client_last_inital;
+  printf("Enter client first name: ");
+  scanf("%s", client_first_name);
+  printf("%s", client_first_name);
 }
