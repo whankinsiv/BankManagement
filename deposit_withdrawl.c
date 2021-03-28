b3e7faad51cfb0c8affabd8abf368904032ea1fe
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2021-03-28 14:49:50 -0700

    Update deposit_withdrawl.c

--- deposit_withdrawl.c
+++ deposit_withdrawl.c
@@ -86,7 +86,7 @@ void deposit_withdrawl(){
     /* Method to format time found on stack exchange */
     assert(strftime(current_time, sizeof(current_time), "%c", tm));
     x = strlen(client_first_name) - 4;
-    client_first_name[x] = '\0';
+    client_first_name[x] = '0';
     printf("%s", client_first_name);
       
     printf("1. Return to main menu\n");
