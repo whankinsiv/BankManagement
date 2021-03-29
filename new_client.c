4f5c4567b12f8ca3791b519dd8085ad33c4cadc1
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2021-03-28 18:25:31 -0700

    Update new_client.c

--- new_client.c
+++ new_client.c
@@ -14,35 +14,65 @@ void new_client(){
   char client_email_address[30];
   int client_age;
   int x;
+  char client_select[10];
+  int client_select_num;
+  int existing_client;
   
   /* Gather information about client and store in respective variables. */
-  printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
-  printf("Please answer the following questions regaurding the new client.\n");
-  printf("Client first name: ");
-  scanf("%s", client_first_name);
-  printf("Client last initial: ");
-  scanf("%s", client_last_inital);
-  printf("Client phone number: ");
-  scanf("%d", &client_phone_number);
-  printf("Client email address: ");
-  scanf("%s", client_email_address);
-  printf("Client age: ");
-  scanf("%d", &client_age);
-  
-  /* Get correct file names to be generated for the client */
-  strcat(strcat(client_first_name, "_"),client_last_inital);
-  x = strlen(client_first_name);
-  for (;x>=0; x--){
-    client_first_name[x] = tolower(client_first_name[x]);
-  }
-  strcpy(client_balance_file_name, client_first_name);
-  strcpy(client_history_file_name, client_first_name);
-  strcpy(client_personal_file_name, client_first_name);
-  strcat(client_balance_file_name, "_balance.txt");
-  strcat(client_history_file_name, "_history.txt");
-  strcat(client_personal_file_name, "_info.txt");
-  printf("%s", client_balance_file_name);
-  printf("%s", client_history_file_name);
-  printf("%s", client_personal_file_name);
+  do{
+    printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+    printf("Please answer the following questions regaurding the new client.\n");
+    printf("Client first name: ");
+    scanf("%s", client_first_name);
+    printf("Client last initial: ");
+    scanf("%s", client_last_inital);
+    printf("Client phone number: ");
+    scanf("%d", &client_phone_number);
+    printf("Client email address: ");
+    scanf("%s", client_email_address);
+    printf("Client age: ");
+    scanf("%d", &client_age);
+
+    /* Get correct file names to be generated for the client */
+    strcat(strcat(client_first_name, "_"),client_last_inital);
+    x = strlen(client_first_name);
+    for (;x>=0; x--){
+      client_first_name[x] = tolower(client_first_name[x]);
+    }
+    strcpy(client_balance_file_name, client_first_name);
+    strcpy(client_history_file_name, client_first_name);
+    strcpy(client_personal_file_name, client_first_name);
+    strcat(client_balance_file_name, "_balance.txt");
+    strcat(client_history_file_name, "_history.txt");
+    strcat(client_personal_file_name, "_info.txt");
+
+    /* Check if a balance file already exists under this name to see if the client already has an account. */
+    client_balance_file = fopen(client_balance_file_name, "r");
+    existing client = 0;
+    while (client_balance_file != NULL){
+      printf("Error. Client this this name already exists.\n");
+      printf("1. Input different client information\n");
+      printf("2. Return to main menu\n");
+      printf("Please select from the above options: ");
+      scanf("%s", client_select);
+      client_select_num = atoi(client_select);
+      while ((client_select_num < 1) || (client_select_num > 2)){
+        printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+        printf("Invalid input. Please try again.\n");
+        printf("1. Input different client information\n");
+        printf("2. Return to main menu\n");
+        printf("Please select from the above options: ");
+        scanf("%s", client_select);
+        client_select_num = atoi(client_select);
+      }
+
+    }
+    if (client_select_num == 2){
+        return;
+      }
+    if (client_select_num == 1){
+      existing_client == 1;
+    }
+  } while (existing_client == 1)
  
 }
