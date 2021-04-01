b5a39dc792303c5e7683e9fc26b77ded6b600144
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2021-03-31 19:43:27 -0700

    Update personal_info.c

--- personal_info.c
+++ personal_info.c
@@ -1,4 +1,22 @@
 #include <stdio.h>
 void personal_info(){
-  printf("Inside personal_info.c");
+  char client_first_name[30];
+  char client_last_inital[30];
+  
+  printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
+  printf("Personal Info Search:\n");
+  printf("Enter client first name: ");
+  scanf("%s", client_first_name);
+  printf("Enter client last inital: ");
+  scanf("%s", client_last_inital);
+  /* Use strcat to combine first name / last inital with and underscore in the middle */
+  strcat(strcat(client_first_name, "_"),client_last_inital);
+  /* Use the tolower function to get the string in lower case. */
+  x = strlen(client_first_name);
+  for (;x>=0; x--){
+    client_first_name[x] = tolower(client_first_name[x]);
+  }
+  strcat(client_first_name, "_info.txt");
+  printf("%s", client_first_name);
+  
 }
