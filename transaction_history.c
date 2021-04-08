4efe7434dfc442d13db2988c0c117e2e68c1e751
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2021-04-07 22:47:30 -0700

    Update transaction_history.c

--- transaction_history.c
+++ transaction_history.c
@@ -1,4 +1,19 @@
 #include <stdio.h>
 void transaction_history(){
-  printf("Inside transaction_history.c");
+  int repeat_script = 1;
+  char client_first_name[30];
+  char client_last_inital[30];
+  int x;
+  printf("Enter client first name: ");
+  scanf("%s", client_first_name);
+  printf("Enter client last inital: ");
+  scanf("%s", client_last_inital);
+  strcat(strcat(client_first_name, "_"),client_last_inital);
+  x = strlen(client_first_name);
+  for (;x>=0; x--){
+    client_first_name[x] = tolower(client_first_name[x]);
+  }
+  strcat(client_first_name, "_history.txt");
+  printf("%s", client_first_name);
+  
 }
