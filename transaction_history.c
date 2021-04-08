aaece842afdc1dcc5da5de38dad510b4f329ce00
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2021-04-07 23:28:11 -0700

    Update transaction_history.c

--- transaction_history.c
+++ transaction_history.c
@@ -44,7 +44,7 @@ void transaction_history(){
   while (fscanf(client_history_file, "%s" ,current_file_line) != EOF){
     printf("%s ", current_file_line);
     x++;
-    if (x==19){
+    if (x==15){
       printf("\n");
       x=0;
     }
