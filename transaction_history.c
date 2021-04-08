0a76b11a551996260db93cc72ff9a2577b755d3b
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2021-04-07 22:55:55 -0700

    Update transaction_history.c

--- transaction_history.c
+++ transaction_history.c
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <ctype.h>
 void transaction_history(){
+  FILE* client_history_file;
   int repeat_script = 1;
   char client_first_name[30];
   char client_last_inital[30];
@@ -18,6 +19,25 @@ void transaction_history(){
     client_first_name[x] = tolower(client_first_name[x]);
   }
   strcat(client_first_name, "_history.txt");
-  printf("%s", client_first_name);
+  client_history_file = fopen(client_first_name, "r");
+  while (client_file == NULL){
+      printf("Unable to locate client, please try again.\n");
+      printf("Enter Q to quit search\n");
+      printf("Enter client first name: ");
+      scanf("%s", client_first_name);
+      if ((client_first_name[0] == 'q') || (client_first_name[0] == 'Q')){
+          return;
+      }
+      printf("Enter client last inital: ");
+      scanf("%s", client_last_inital);
+      strcat(strcat(client_first_name, "_"),client_last_inital);
+      x = strlen(client_first_name);
+      for (;x>=0; x--){
+        client_first_name[x] = tolower(client_first_name[x]);
+      }
+      strcat(client_transaction_history_name, "_history.txt");
+      
+      client_history_file = fopen(client_first_name, "r");
+  }
   
 }
