cec70ad4439f1bd3941b7862359982db657b3709
William Hankins <75007804+whankinsiv@users.noreply.github.com>
2021-04-12 15:00:54 -0700

    Update transaction_history.c

--- transaction_history.c
+++ transaction_history.c
@@ -11,7 +11,9 @@ void transaction_history(){
   char client_last_inital[30];
   char current_file_line[100];
   int x;
+  /* Continue running the script until the user requests to return to the main menu when prompted. */
   while (repeat_script == 1){
+    /* Prompt user for the client's name and concatinate it with _history.txt to get the correct file name. */
     printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
     printf("Enter client first name: ");
     scanf("%s", client_first_name);
@@ -23,12 +25,15 @@ void transaction_history(){
       client_first_name[x] = tolower(client_first_name[x]);
     }
     strcat(client_first_name, "_history.txt");
+    /* Open the client's history file with read access. */
     client_history_file = fopen(client_first_name, "r");
+    /* If the client's history file does not exist, prompt user to try again with a different name. */
     while (client_history_file == NULL){
         printf("Unable to locate client, please try again.\n");
         printf("Enter Q to quit search\n");
         printf("Enter client first name: ");
         scanf("%s", client_first_name);
+        /* If the user enters q for the first name, return to the main menu. */
         if ((client_first_name[0] == 'q') || (client_first_name[0] == 'Q')){
             return;
         }
@@ -44,6 +49,7 @@ void transaction_history(){
         client_history_file = fopen(client_first_name, "r");
     }
     x=0;
+    /* Once the client history file has been sucessfully opened, print the text file to the screen. */
     while (fscanf(client_history_file, "%s" ,current_file_line) != EOF){
       printf("%s ", current_file_line);
       x++;
@@ -52,12 +58,15 @@ void transaction_history(){
         x=0;
       }
     }
+    /* Close the text file. */
     fclose(client_history_file);
+    /* Ask user if they would like to repeat the function or return to the main menu. */
     printf("\n1. Search another client's history\n");
     printf("2. Return to main menu\n");
     printf("Please select from the above options: ");
     scanf("%s", client_select);
     client_select_num = atoi(client_select);
+    /* While the user reponse is invalid, continue to prompt for a valid input. */
     while ((client_select_num < 1) || (client_select_num > 2)){
       printf("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
       printf("Invalid input. Please try again.\n");
@@ -67,9 +76,11 @@ void transaction_history(){
       scanf("%s", client_select);
       client_select_num = atoi(client_select);
     }
+    /* If the user selects 2, return to the main menu. */
     if (client_select_num == 2){
       return;
     }
     repeat_script = 1;
+    /* If the user selects 1, repeat the function. */
   }
 }
